4d28322083051a243749a4104d09c27f4f53ff19
Holycitysociety <99046407+Holycitysociety@users.noreply.github.com>
2025-11-30 01:42:20 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,24 +2,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { PrivyProvider } from "@privy-io/react-auth";
 import { ThirdwebProvider } from "thirdweb/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* ThirdwebProvider sets up QueryClient + context for CheckoutWidget */}
     <ThirdwebProvider clientId="f58c0bfc6e6a2c00092cc3c35db1eed8">
-      <PrivyProvider
-        appId="cmijg24lf00s4ju0cfg87p8nw"
-        config={{
-          loginMethods: ["email", "sms", "google"],
-          appearance: {
-            theme: "dark",
-          },
-        }}
-      >
-        <App />
-      </PrivyProvider>
+      <App />
     </ThirdwebProvider>
   </React.StrictMode>
 );

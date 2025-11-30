04ebc8fb4ca0c6c3d01cd5c26a03c16a84d5d477
Holycitysociety <99046407+Holycitysociety@users.noreply.github.com>
2025-11-29 20:24:05 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -122,6 +122,23 @@ export default function App() {
           {/* Thirdweb Checkout visible on the page */}
           <CheckoutBoundary>
             
+            <CheckoutWidget
+  client={client}
+  description={
+    "USPPA, COWBOY POLO CIRCUIT, CHARLESTON POLO's PATRONAGE UTILITY TOKEN"
+  }
+  name={"POLO PATRONIUM"}
+  currency={"USD"}
+  chain={defineChain(8453)}
+  amount={"1"}
+  tokenAddress={"0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913"}
+  seller={"0xfee3c75691e8c10ed4246b10635b19bfff06ce16"}
+  buttonLabel={"ADD USD TO YOUR PATRON WALLET"}
+  onError={(err) => {
+    console.error("Checkout error:", err);
+    alert(err?.message || String(err));
+  }}
+/>
             
           </CheckoutBoundary>
 

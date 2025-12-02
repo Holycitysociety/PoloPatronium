c0b17f7e30f7809e61fed17bd35a525d26a9800d
Holycitysociety <99046407+Holycitysociety@users.noreply.github.com>
2025-12-02 08:16:21 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -444,7 +444,7 @@ export default function App() {
                 <div className="rule" />
                 <div className="string-middle">
                   <span className="string-side">THREE  </span>
-                  <span className="sevens">  7̶7̶7̶  </span>
+                  <span className="sevens">   7̶7̶7̶  </span>
                   <span className="string-side">  SEVENS</span>
                 </div>
                 <div className="remuda-bottom">REMUDA</div>
